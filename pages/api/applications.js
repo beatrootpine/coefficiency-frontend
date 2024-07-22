2370341f8d273794bf9863c6d77171889ebe9c8c
Thembekile Moshoane <thembi@Thembekiles-MacBook-Pro.local>
2024-07-23 00:28:20 +0200

    Update API endpoint URL to Heroku app

--- pages/api/applications.js
+++ pages/api/applications.js
@@ -4,8 +4,8 @@ export default async function handler(req, res) {
   const { userId } = req.query;
 
   try {
-    const response = await axios.get(`https://your-heroku-app.herokuapp.com/api/applications`, {
-      params: { userId }
+    const response = await axios.get(`https://coefficiency-39fead0059f9.herokuapp.com/api/applications`, {
+      params: { userId },
     });
 
     res.status(200).json(response.data);
