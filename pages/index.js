b08c83221a430ce06d2420dfa7b13dc8b27b8af9
Malume Business <128156673+beatrootpine@users.noreply.github.com>
2024-07-23 01:43:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+
 import Link from 'next/link';
 
 export default function Home() {
